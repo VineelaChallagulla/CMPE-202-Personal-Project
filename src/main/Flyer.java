f8501e9f2dac50676cfe6aef149845f7e746e62d
VineelaChallagulla <vineelachallagulla@gmail.com>
2017-03-18 23:26:23 -0700

    Sample code to parse

---
+++ src/main/Flyer.java
@@ -0,0 +1,15 @@
+package oop;
+
+
+
+public class Flyer {
+	
+	
+	public void  makeFly(Bird bird){
+		
+		System.out.println("I do some extra procesing which birds are not capble of doing  then also ask birds to fly after or before i have done my shit");
+		
+		bird.fly();
+	}
+
+}
