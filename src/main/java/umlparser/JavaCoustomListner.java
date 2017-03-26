a1be6bdb94bb6a414e21bcd68be85d343a8dcfb2
VineelaChallagulla <vineelachallagulla@gmail.com>
2017-03-25 21:44:15 -0700

    Changing from Java8 to Java
    Changing from Java8 base listener to Java listner

--- src/main/java/umlparser/JavaCoustomListner.java
+++ src/main/java/umlparser/JavaCoustomListner.java
@@ -1,87 +1,129 @@
 package umlparser;
 
+import java.util.ArrayList;
+
 import org.antlr.v4.runtime.TokenStream;
 
-import cmpe202.project.Java8BaseListener;
-import cmpe202.project.Java8Parser;
-import cmpe202.project.Java8Parser.FormalParameterListContext;
-import cmpe202.project.Java8Parser.MethodDeclaratorContext;
-import cmpe202.project.Java8Parser.MethodHeaderContext;
+import cmpe202.project.JavaBaseListener;
+import cmpe202.project.JavaParser;
+import cmpe202.project.JavaParser.TypeContext;
+import cmpe202.project.JavaParser.VariableDeclaratorContext;
 
-public class JavaCoustomListner extends Java8BaseListener {
-	
-    Java8Parser parser;
-    public JavaCoustomListner(Java8Parser parser) {this.parser = parser;}
-	
-	@Override public void enterMethodDeclaration(Java8Parser.MethodDeclarationContext ctx) {
-		
-		MethodHeaderContext methodHeader =  ctx.methodHeader();
-		MethodDeclaratorContext methodDeclarator = methodHeader.methodDeclarator();
-		String methodName = methodDeclarator.Identifier().getText();
-		System.out.println(methodName+"()");
-		
+public class JavaCoustomListner extends JavaBaseListener {
+
+	JavaParser parser;
+
+	public JavaCoustomListner(JavaParser parser) {
+		this.parser = parser;
 	}
-	
-	
+
 	/**
 	 * {@inheritDoc}
 	 *
-	 * <p>The default implementation does nothing.</p>
+	 * <p>
+	 * The default implementation doesvftfv nothing.
+	 * </p>
 	 */
-	@Override public void exitMethodDeclaration(Java8Parser.MethodDeclarationContext ctx) {
-		
-		
-		
-		
+	@Override
+	public void enterPackageDeclaration(JavaParser.PackageDeclarationContext ctx) {
+
+		System.out.println(ctx.qualifiedName().Identifier());
 	}
-	
-	
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void enterClassDeclaration(Java8Parser.ClassDeclarationContext ctx) {
-	
-		
+
+	@Override
+	public void enterMethodDeclaration(JavaParser.MethodDeclarationContext ctx) { // need
+																					// parser
+																					// to
+																					// get
+																					// tokens
+		TokenStream tokens = parser.getTokenStream();
+		String type = "void";
+		if (ctx.type() != null) {
+			type = tokens.getText(ctx.type());
+		}
+		String args = tokens.getText(ctx.formalParameters());
+		System.out.println("\t" + type + " " + ctx.Identifier() + args + ";");
 	}
+
 	/**
 	 * {@inheritDoc}
 	 *
-	 * <p>The default implementation does nothing.</p>
+	 * <p>
+	 * The default implementation does nothing.
+	 * </p>
 	 */
-	@Override public void exitClassDeclaration(Java8Parser.ClassDeclarationContext ctx) { }
-	
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void enterNormalClassDeclaration(Java8Parser.NormalClassDeclarationContext ctx) {
-		System.out.println("class "+ctx.Identifier()+" {");
-		
+	@Override
+	public void exitMethodDeclaration(JavaParser.MethodDeclarationContext ctx) {
+
 	}
+
 	/**
 	 * {@inheritDoc}
 	 *
-	 * <p>The default implementation does nothing.</p>
+	 * <p>
+	 * The default implementation does nothing.
+	 * </p>
 	 */
-	@Override public void exitNormalClassDeclaration(Java8Parser.NormalClassDeclarationContext ctx) {
+	@Override
+	public void enterClassDeclaration(JavaParser.ClassDeclarationContext ctx) {
+
+		TokenStream tokens = parser.getTokenStream();
+		String type = "void";
+		if (ctx.type() != null) {
+			type = tokens.getText(ctx.type());
+		}
+
+		ArrayList<String> implementList = new ArrayList<String>();
+		if (ctx.typeList() != null) {
+			for (TypeContext interfaceType : ctx.typeList().type()) {
+				implementList.add(tokens.getText(interfaceType));
+			}
+		}
+
+		System.out.println("class " + ctx.Identifier() + "extends" + type + "implements" + implementList + " {");
+
+	}
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>
+	 * The default implementation does nothing.
+	 * </p>
+	 */
+	@Override
+	public void exitClassDeclaration(JavaParser.ClassDeclarationContext ctx) {
+
 		System.out.println("}");
 	}
-	
+
 	/**
 	 * {@inheritDoc}
 	 *
-	 * <p>The default implementation does nothing.</p>
+	 * <p>
+	 * The default implementation does nothing.
+	 * </p>
 	 */
-	@Override public void enterMethodModifier(Java8Parser.MethodModifierContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void exitMethodModifier(Java8Parser.MethodModifierContext ctx) { }
-	
-	
+	@Override
+	public void enterFieldDeclaration(JavaParser.FieldDeclarationContext ctx) {
+
+		TokenStream tokens = parser.getTokenStream();
+		String variableType;
+		if (ctx.type() != null) {
+
+			variableType = tokens.getText(ctx.type());
+
+			if (variableType.contains("[")) {
+
+			}
+			System.out.println(variableType);
+		}
+
+		for (VariableDeclaratorContext variableDeclaratorContext : ctx.variableDeclarators().variableDeclarator()) {
+			System.out.println(variableDeclaratorContext.variableDeclaratorId().Identifier());
+
+		}
+
+	}
+
 }
