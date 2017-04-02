b36969590e3454494f1ef9fea765f2c355e2cd69
VineelaChallagulla <vineelachallagulla@gmail.com>
2017-04-01 23:14:09 -0700

    Modifying variables definition

--- src/main/java/umlparser/ClassDefinition.java
+++ src/main/java/umlparser/ClassDefinition.java
@@ -1,53 +1,77 @@
 package umlparser;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ClassDefinition {
 	private String packageName;
 	private String name;
 	private String extendsClassName;
-	private String implementsInterfaceName;
-	private ArrayList<String> variables = new ArrayList<String>();
+	private ArrayList<String> implementInterfaceNames;
+	private Map<String, String> variables = new HashMap<String, String>();
 	private ArrayList<String> methodSignatures = new ArrayList<String>();
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getExtendsClassName() {
 		return extendsClassName;
 	}
+
 	public void setExtendsClassName(String extendsClassName) {
 		this.extendsClassName = extendsClassName;
 	}
-	public String getImplementsInterfaceName() {
-		return implementsInterfaceName;
-	}
-	public void setImplementsInterfaceName(String implementsInterfaceName) {
-		this.implementsInterfaceName = implementsInterfaceName;
-	}
-	public ArrayList<String> getVariables() {
+
+	public Map<String, String> getVariables() {
 		return variables;
 	}
-	public void setVariables(ArrayList<String> variables) {
+
+	public void setVariables(Map<String, String> variables) {
 		this.variables = variables;
 	}
+
+	public void addVariable(String variable, String type) {
+		this.variables.put(variable, type);
+	}
+
 	public ArrayList<String> getMethodSignatures() {
 		return methodSignatures;
 	}
+
 	public void setMethodSignatures(ArrayList<String> methodSignatures) {
 		this.methodSignatures = methodSignatures;
 	}
+
 	public void addMethodSignature(String methodSignature) {
 		this.methodSignatures.add(methodSignature);
 	}
+
 	public String getPackageName() {
 		return packageName;
 	}
+
 	public void setPackageName(String packageName) {
 		this.packageName = packageName;
 	}
+
+	public ArrayList<String> getImplementInterfaceNames() {
+		return implementInterfaceNames;
+	}
+
+	public void setImplementInterfaceNames(ArrayList<String> implementInterfaceNames) {
+		this.implementInterfaceNames = implementInterfaceNames;
+	}
+
+	public void addImplementInterfaceName(String implementInterfaceName) {
+		this.implementInterfaceNames.add(implementInterfaceName);
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -56,6 +80,7 @@ public class ClassDefinition {
 		result = prime * result + ((packageName == null) ? 0 : packageName.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
