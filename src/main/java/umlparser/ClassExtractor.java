a7e915fcb7c3a5feeab74b56cc80bfc8659bb9d0
VineelaChallagulla <vineelachallagulla@gmail.com>
2017-04-08 23:39:40 -0700

    Modifying Class extractor
    Now extractClassDefinition returns ClassDefinition bean.

--- src/main/java/umlparser/ClassExtractor.java
+++ src/main/java/umlparser/ClassExtractor.java
@@ -1,42 +1,36 @@
 package umlparser;
 
-
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 
 import org.antlr.v4.runtime.tree.*;
 
-import cmpe202.project.JavaBaseListener;
 import cmpe202.project.JavaLexer;
 import cmpe202.project.JavaParser;
 
-
-import java.io.InputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class ClassExtractor {
-    public static void main(String[] args) throws Exception {
-        String inputFile = null;
-        if ( args.length>0 ) inputFile = args[0];
-        InputStream is = System.in;
-//        if ( inputFile!=null ) {
-//            is = new FileInputStream(inputFile);
-//        }
-        
-        is = ClassExtractor.class.getResourceAsStream("test.properties");
-        ANTLRInputStream input = new ANTLRInputStream(is);
+	public ClassDefinition extractClassDefinition(String filePath) throws FileNotFoundException, IOException {
 
-        JavaLexer lexer = new JavaLexer(input);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        JavaParser parser = new JavaParser(tokens);
-        ParseTree tree = parser.compilationUnit(); // parse
+		File file = new File(filePath);
+		ANTLRInputStream input = new ANTLRInputStream(new FileReader(file));
 
-        ParseTreeWalker walker = new ParseTreeWalker(); // create standard walker
-        JavaCoustomListner extractor = new JavaCoustomListner(parser);
-        ClassDefinition classDefinition = new ClassDefinition();
-        extractor.setClassDefinition(classDefinition);
-        walker.walk(extractor, tree); // initiate walk of tree with listener     
-        ClassDefinitionsToPlantUmlTransformer ClassDefinitionsToPlantUmlTransformer = new ClassDefinitionsToPlantUmlTransformer();
-        System.out.println(ClassDefinitionsToPlantUmlTransformer.getTransformedClassDefinition(classDefinition));
-        
-    }
+		JavaLexer lexer = new JavaLexer(input);
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+		JavaParser parser = new JavaParser(tokens);
+		ParseTree tree = parser.compilationUnit(); // parse
+
+		ParseTreeWalker walker = new ParseTreeWalker(); // create standard
+														// walker
+		JavaCoustomListner extractor = new JavaCoustomListner(parser);
+		ClassDefinition classDefinition = new ClassDefinition();
+		extractor.setClassDefinition(classDefinition);
+		walker.walk(extractor, tree); // initiate walk of tree with listener
+		return classDefinition;
+
+	}
 }
