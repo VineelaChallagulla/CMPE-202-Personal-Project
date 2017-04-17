dc6f267f4bfdbf8ff6597b8f39dfdd54764dcb1c
VineelaChallagulla <vineelachallagulla@gmail.com>
2017-04-17 01:04:35 -0700

    Update ClassRelations.java

--- src/main/java/umlparser/ClassRelations.java
+++ src/main/java/umlparser/ClassRelations.java
@@ -9,9 +9,9 @@ public enum ClassRelations {
 	
 	private Map<String, List<String>> implementsMap = new HashMap<>();
 	
-	private Map<String, List<Map<String, Integer>>> compositionMap = new HashMap<>();
+	private Map<String, List<Association>> compositionMap = new HashMap<>();
 	
-	private Map<String, List<Map<String, Integer>>> aggregationMap = new HashMap<>();	
+	private Map<String, List<Association>> aggregationMap = new HashMap<>();	
 	
 	private Map<String, String> extendsMap = new HashMap<>();
 	
@@ -21,22 +21,58 @@ public enum ClassRelations {
 		
 	}
 	
-	void addCompositionRelation(String className, List<Map<String, Integer>> compositionRelation){
+	void addCompositionRelation(String className, List<Association> compositionRelation){
 		compositionMap.put(className, compositionRelation);	
 		
 	}
 	
 	
 	
-	void aggregationRelation(String className, List<Map<String, Integer>> aggregationRelation){
+	void addAggregationRelation(String className, List<Association> aggregationRelation){
 		aggregationMap.put(className, aggregationRelation);	
 		
 	}
 	
 
-	void addextendsRelation(String className, String extendedClass){
+	void addExtendsRelation(String className, String extendedClass){
 		extendsMap.put(className, extendedClass);	
 		
 	}
+
+	public Map<String, List<String>> getImplementsMap() {
+		return implementsMap;
+	}
+
+	public void setImplementsMap(Map<String, List<String>> implementsMap) {
+		this.implementsMap = implementsMap;
+	}
+
+	public Map<String, List<Association>> getCompositionMap() {
+		return compositionMap;
+	}
+
+	public void setCompositionMap(Map<String, List<Association>> compositionMap) {
+		this.compositionMap = compositionMap;
+	}
+
+	public Map<String, List<Association>> getAggregationMap() {
+		return aggregationMap;
+	}
+
+	public void setAggregationMap(Map<String, List<Association>> aggregationMap) {
+		this.aggregationMap = aggregationMap;
+	}
+
+	public Map<String, String> getExtendsMap() {
+		return extendsMap;
+	}
+
+	public void setExtendsMap(Map<String, String> extendsMap) {
+		this.extendsMap = extendsMap;
+	}
+	
+
+
+
 	
 }
