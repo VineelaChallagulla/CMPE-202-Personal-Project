fa2bb8979fd9bc7b9d25a47e61dbd38bfd3553a2
VineelaChallagulla <vineelachallagulla@gmail.com>
2017-04-17 01:02:35 -0700

    Create Association.java

---
+++ src/main/java/umlparser/Association.java
@@ -0,0 +1,27 @@
+package umlparser;
+
+public class Association {
+	
+	private String Name;
+	private String type;
+	private String number;
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	
+}
