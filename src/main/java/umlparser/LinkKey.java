137677731d2961d266fbd94906338402c8f1c023
VineelaChallagulla <vineelachallagulla@gmail.com>
2017-04-18 01:50:12 -0700

    Create LinkKey.java

---
+++ src/main/java/umlparser/LinkKey.java
@@ -0,0 +1,46 @@
+package umlparser;
+
+public class LinkKey {
+	
+	private String source;
+	private String target;
+	public String getSource() {
+		return source;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
+	public String getTarget() {
+		return target;
+	}
+	public void setTarget(String target) {
+		this.target = target;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((source == null) ? 0 : source.hashCode());
+		result = prime * result + ((target == null) ? 0 : target.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		LinkKey other = (LinkKey) obj;
+	if(other.source.equals(source) && other.target.equals(target)|| other.target.equals(source) && other.source.equals(target)){
+		return true;
+	}else{
+		return false;
+	}
+
+
+}
+
+}
