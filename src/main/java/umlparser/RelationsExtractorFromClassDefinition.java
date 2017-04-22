e6c08fdf0eeec364822c46a4f0720b797e642063
VineelaChallagulla <vineelachallagulla@gmail.com>
2017-04-22 02:20:42 -0700

    Update RelationsExtractorFromClassDefinition.java

--- src/main/java/umlparser/RelationsExtractorFromClassDefinition.java
+++ src/main/java/umlparser/RelationsExtractorFromClassDefinition.java
@@ -16,54 +16,85 @@ public class RelationsExtractorFromClassDefinition {
 	private static final String  GENERIC_REGEX = "(\\w*)\\s*<\\s*(\\w*)\\s*>";
 	private static final Pattern GENERIC_PATTERN =  Pattern.compile(GENERIC_REGEX);
 
-	public enum Primitive {
-		Boolean, Char, Byte, Short, Int, Long, Float, Double
-	}
 
 	public ClassRelations extractRelations(ClassDefinition classDefinition) {
 		ClassRelations classRelations = ClassRelations.INSTANCE;
 		classRelations.addImplementationRelation(classDefinition.getName(),
 				classDefinition.getImplementInterfaceNames());
 		classRelations.addExtendsRelation(classDefinition.getName(), classDefinition.getExtendsClassName());
+		setAssociationRelation(classDefinition,  classRelations);
 		setAggregationRelation(classDefinition,  classRelations);
+		setCompositionRelation(classDefinition,  classRelations);
 		return classRelations;
 
 	}
 	
-	private void setAggregationRelation(ClassDefinition classDefinition, ClassRelations classRelations){		
-		 List<Association>  aggregationRelationList = new ArrayList<>();
+	private void setAssociationRelation(ClassDefinition classDefinition, ClassRelations classRelations){		
+		 List<Association>  associationList = new ArrayList<>();
+		 
 		for (Entry<String, String> entry: classDefinition.getVariables().entrySet()){
-			Association association = new Association();
-			Matcher arrayMatcher = ARRAY_PATTERN.matcher(entry.getValue().toString());
-			if(arrayMatcher.matches()){
-				String number = null;
-				String type = arrayMatcher.group(1);
-				if(arrayMatcher.group(2) != null){
-					number =  arrayMatcher.group(2);
-				}else{
-					
-					number = "*";
-				}
-				association.setType(type);
-				association.setNumber(number);
-				association.setName(entry.getKey());
+			setAssociation(associationList, entry);
+		}	
+		
 
-			}else{
-			Matcher genericMatcher = GENERIC_PATTERN.matcher(entry.getValue().toString());
-			if(genericMatcher.matches()){
-				String type; 
-				if(genericMatcher.group(2) != null){
-				type = genericMatcher.group(2);
-				association.setType(type);
-				association.setNumber("*");
-				association.setName(entry.getKey());
-				}			
-
-			}
-			}
-			aggregationRelationList.add(association);
-		}
-		classRelations.addAggregationRelation(classDefinition.getName(), aggregationRelationList);
+		classRelations.addAssociationRelation(classDefinition.getName(), associationList);
 	}
+	
+	private void setAggregationRelation(ClassDefinition classDefinition, ClassRelations classRelations){		
+		 List<Association>  aggregationList = new ArrayList<>();
+		
+		for (Entry<String, String> entry: classDefinition.getMethodVariables().entrySet()){
+			setAssociation(aggregationList, entry);
+		}	
+
+		classRelations.addAggregationRelation(classDefinition.getName(), aggregationList);
+	}
+	
+	private void setCompositionRelation(ClassDefinition classDefinition, ClassRelations classRelations){		
+		 List<Association>  compositionList = new ArrayList<>();
+		
+		for (Entry<String, String> entry: classDefinition.getConstructorVariables().entrySet()){
+			setAssociation(compositionList, entry);
+		}	
+
+		classRelations.addCompositionRelation(classDefinition.getName(), compositionList);
+	}
+
+	public void setAssociation(List<Association> associationList, Entry<String, String> entry) {
+		Matcher genericMatcher = GENERIC_PATTERN.matcher(entry.getValue().toString());
+		Association association = new Association();
+		Matcher arrayMatcher = ARRAY_PATTERN.matcher(entry.getValue().toString());
+		if(arrayMatcher.matches()){
+			String number = null;
+			String type = arrayMatcher.group(1);
+			if(arrayMatcher.group(2) != null && ! arrayMatcher.group(2).isEmpty()){
+				number =  arrayMatcher.group(2);
+			}else{
+				
+				number = "many";
+			}
+			association.setType(type);
+			association.setNumber(number);
+			association.setName(entry.getKey());
+
+		}else if(genericMatcher.matches()){
+			String type; 
+			if(genericMatcher.group(2) != null){
+			type = genericMatcher.group(2);
+			association.setType(type);
+			association.setNumber("many");
+			association.setName(entry.getKey());
+			}			
+
+		}			
+		else{
+			association.setType(entry.getValue());
+			association.setName(entry.getKey());
+			
+		}
+		associationList.add(association);
+	}
+	
+
 
 }
