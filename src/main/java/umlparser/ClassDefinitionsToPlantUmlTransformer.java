078df5cee4ba6108cbddd7fa758e00f323b705ef
VineelaChallagulla <vineelachallagulla@gmail.com>
2017-04-22 02:18:41 -0700

    Update ClassDefinitionsToPlantUmlTransformer.java

--- src/main/java/umlparser/ClassDefinitionsToPlantUmlTransformer.java
+++ src/main/java/umlparser/ClassDefinitionsToPlantUmlTransformer.java
@@ -55,7 +55,7 @@ A 0 *-- many y
 
 public class ClassDefinitionsToPlantUmlTransformer {	
 	
-	private static final String AGGREGATION = "0--";
+	private static final String AGGREGATION = "--";
 	private static final String COMPOSITION = "*--";
 	private static final String EXTENDS = "---|>";
 	private static final String IMPLEMENTS = "....|>";
@@ -80,6 +80,8 @@ public class ClassDefinitionsToPlantUmlTransformer {
 //			System.out.println(classDefinition.toString());
 		}
 		stringBuffer.append(getAssociations());
+		stringBuffer.append(getAggregations());
+		stringBuffer.append(getCompositions());
 		stringBuffer.append("@enduml"+ "\n");
 		return stringBuffer.toString();
 		
@@ -118,8 +120,13 @@ public class ClassDefinitionsToPlantUmlTransformer {
 		if (classDefinition.getImplementInterfaceNames() !=null &&!classDefinition.getImplementInterfaceNames().isEmpty()){
 			buffer.append(getImplementsSyntax(classDefinition));
 		}
-		
-		populateAssociations(classDefinition);
+		ClassRelations classRelations = ClassRelations.INSTANCE;
+		Map<String, List<Association>> aggregationMap = classRelations.getAggregationMap();
+		populateAggregations(classDefinition, aggregationMap, classRelations);
+		Map<String, List<Association>> associationMap = classRelations.getAssociationMap();
+		populateAssociations(classDefinition, associationMap, classRelations);
+		Map<String, List<Association>> compositionMap = classRelations.getCompositionMap();
+		populateCompositions(classDefinition, compositionMap, classRelations);
 		
 		
 		return buffer.toString();	
@@ -192,12 +199,12 @@ public class ClassDefinitionsToPlantUmlTransformer {
 		return isPrimitive;
 	}
 	
-	public void  populateAssociations( ClassDefinition classDefinition){
-		ClassRelations classRelations = ClassRelations.INSTANCE;
+	public void  populateAssociations( ClassDefinition classDefinition,  Map<String, List<Association>>  map, ClassRelations classRelations){
+
 		StringBuffer buffer = new StringBuffer();
 		
 		
-		Map<String, List<Association>> map = classRelations.getAggregationMap();
+
 		
 		for(Entry<String, List<Association>> entry: map.entrySet()){
 			
@@ -236,6 +243,95 @@ public class ClassDefinitionsToPlantUmlTransformer {
 	
 }
 	
+	public void  populateAggregations( ClassDefinition classDefinition,  Map<String, List<Association>>  map, ClassRelations classRelations){
+
+		StringBuffer buffer = new StringBuffer();
+		
+		
+
+		
+		for(Entry<String, List<Association>> entry: map.entrySet()){
+			
+			for(Association association: entry.getValue()){
+				if (!isPrimitive(association.getType())){
+				
+				Link link= new Link();
+				LinkKey linkKey= new LinkKey();
+				link.setSource(entry.getKey());
+				link.setTarget(association.getType());
+				link.setTarget_number(association.getNumber());
+				linkKey.setSource(entry.getKey());
+				linkKey.setTarget(association.getType());
+				
+				if(classRelations.getAggregationLinks().get(linkKey) == null){
+					System.out.println("first");
+					System.out.println(link);
+					classRelations.getAggregationLinks().put(linkKey, link);
+				}else{
+					link = classRelations.getAggregationLinks().get(linkKey);
+					link.setSource_number(association.getNumber());
+					System.out.println("second");
+					System.out.println(link);
+					
+				
+			}
+				}
+		}
+
+		
+		
+			
+			
+	}
+
+	
+}
+	
+	public void  populateCompositions( ClassDefinition classDefinition,  Map<String, List<Association>>  map, ClassRelations classRelations){
+
+		StringBuffer buffer = new StringBuffer();
+		
+		
+
+		
+		for(Entry<String, List<Association>> entry: map.entrySet()){
+			
+			for(Association association: entry.getValue()){
+				if (!isPrimitive(association.getType())){
+				
+				Link link= new Link();
+				LinkKey linkKey= new LinkKey();
+				link.setSource(entry.getKey());
+				link.setTarget(association.getType());
+				link.setTarget_number(association.getNumber());
+				linkKey.setSource(entry.getKey());
+				linkKey.setTarget(association.getType());
+				
+				if(classRelations.getCompositionLinks().get(linkKey) == null){
+					System.out.println("first");
+					System.out.println(link);
+					classRelations.getCompositionLinks().put(linkKey, link);
+				}else{
+					link = classRelations.getCompositionLinks().get(linkKey);
+					link.setSource_number(association.getNumber());
+					System.out.println("second");
+					System.out.println(link);
+					
+				
+			}
+				}
+		}
+
+		
+		
+			
+			
+	}
+
+	
+}
+	
+	
 	
 	public String  getAssociations(  ){
 		ClassRelations classRelations = ClassRelations.INSTANCE;
@@ -246,6 +342,38 @@ public class ClassDefinitionsToPlantUmlTransformer {
 		for (Link link:classRelations.getLinks().values()){
 			buffer.append(link.getSource()).append(" ").append('"').append(link.getSource_number() != null?link.getSource_number(): "1").append('"').append(" ");
 			buffer.append(ASSOSCIATION).append(" ").append('"').append(link.getTarget_number()!= null? link.getTarget_number() : "1" ).append('"').append(" ").append(link.getTarget());
+			buffer.append("\n");
+			
+		}
+		return buffer.toString();
+	
+}
+	
+	public String  getAggregations(  ){
+		ClassRelations classRelations = ClassRelations.INSTANCE;
+		StringBuffer buffer = new StringBuffer();
+		
+		
+		/*Class01 "1" *-- "many" Class028*/
+		for (Link link:classRelations.getAggregationLinks().values()){
+			buffer.append(link.getSource()).append(" ").append('"').append(link.getSource_number() != null?link.getSource_number(): "1").append('"').append(" ");
+			buffer.append(AGGREGATION).append(" ").append('"').append(link.getTarget_number()!= null? link.getTarget_number() : "1" ).append('"').append(" ").append(link.getTarget());
+			buffer.append("\n");
+			
+		}
+		return buffer.toString();
+	
+}
+	
+	public String  getCompositions(  ){
+		ClassRelations classRelations = ClassRelations.INSTANCE;
+		StringBuffer buffer = new StringBuffer();
+		
+		
+		/*Class01 "1" *-- "many" Class028*/
+		for (Link link:classRelations.getCompositionLinks().values()){
+			buffer.append(link.getSource()).append(" ").append('"').append(link.getSource_number() != null?link.getSource_number(): "1").append('"').append(" ");
+			buffer.append(COMPOSITION).append(" ").append('"').append(link.getTarget_number()!= null? link.getTarget_number() : "1" ).append('"').append(" ").append(link.getTarget());
 			buffer.append("\n");
 			
 		}
