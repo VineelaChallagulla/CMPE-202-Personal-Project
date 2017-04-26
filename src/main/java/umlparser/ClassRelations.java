ad9c19c93debadca66fb127ee30ef90e9e79a403
VineelaChallagulla <vineelachallagulla@gmail.com>
2017-04-25 17:33:24 -0700

    Update ClassRelations.java

--- src/main/java/umlparser/ClassRelations.java
+++ src/main/java/umlparser/ClassRelations.java
@@ -8,48 +8,44 @@ import java.util.Set;
 
 public enum ClassRelations {
 	INSTANCE;
-	
-	private Map<String, List<String>> implementsMap = new HashMap<>();
-	
-	private Map<String, List<Association>> compositionMap = new HashMap<>();
-	
-	private Map<String, List<Association>> aggregationMap = new HashMap<>();	
-	
-	private Map<String, List<Association>> associationMap = new HashMap<>();	
-	
-	private Map<String, String> extendsMap = new HashMap<>();
-	
-	private Map<LinkKey, Link > links = new HashMap<>();
-	private Map<LinkKey, Link > aggregationLinks = new HashMap<>();
-	private Map<LinkKey, Link > compositionLinks = new HashMap<>();
-	
-	
-	void addImplementationRelation(String className, List<String> implementedClasses){
-		implementsMap.put(className, implementedClasses);	
-		
-	}
-	
-	void addCompositionRelation(String className, List<Association> compositionRelation){
-		compositionMap.put(className, compositionRelation);	
-		
-	}
-	
-	
-	
-	void addAggregationRelation(String className, List<Association> aggregationRelation){
-		aggregationMap.put(className, aggregationRelation);	
-		
-	}
-	
-	void addAssociationRelation(String className, List<Association> associationRelation){
-		associationMap.put(className, associationRelation);	
-		
-	}	
-	
 
-	void addExtendsRelation(String className, String extendedClass){
-		extendsMap.put(className, extendedClass);	
-		
+	private Map<String, List<String>> implementsMap = new HashMap<>();
+
+	private Map<String, List<Association>> compositionMap = new HashMap<>();
+
+	private Map<String, List<Association>> aggregationMap = new HashMap<>();
+
+	private Map<String, List<Association>> associationMap = new HashMap<>();
+
+	private Map<String, String> extendsMap = new HashMap<>();
+
+	private Map<LinkKey, Link> links = new HashMap<>();
+	private Map<LinkKey, Link> aggregationLinks = new HashMap<>();
+	private Map<LinkKey, Link> compositionLinks = new HashMap<>();
+
+	void addImplementationRelation(String className, List<String> implementedClasses) {
+		implementsMap.put(className, implementedClasses);
+
+	}
+
+	void addCompositionRelation(String className, List<Association> compositionRelation) {
+		compositionMap.put(className, compositionRelation);
+
+	}
+
+	void addAggregationRelation(String className, List<Association> aggregationRelation) {
+		aggregationMap.put(className, aggregationRelation);
+
+	}
+
+	void addAssociationRelation(String className, List<Association> associationRelation) {
+		associationMap.put(className, associationRelation);
+
+	}
+
+	void addExtendsRelation(String className, String extendedClass) {
+		extendsMap.put(className, extendedClass);
+
 	}
 
 	public Map<String, List<String>> getImplementsMap() {
@@ -91,6 +87,7 @@ public enum ClassRelations {
 	public void setLinks(Map<LinkKey, Link> links) {
 		this.links = links;
 	}
+
 	public void addLink(LinkKey linkKey, Link link) {
 		this.links.put(linkKey, link);
 	}
@@ -115,12 +112,8 @@ public enum ClassRelations {
 		this.compositionLinks.put(linkKey, link);
 	}
 
-	
 	public void addAggregationLinks(LinkKey linkKey, Link link) {
 		this.aggregationLinks.put(linkKey, link);
 	}
 
-
-
-	
 }
