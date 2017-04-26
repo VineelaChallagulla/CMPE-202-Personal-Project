d25507e1f60e52f9fa7a34201e4b8bf6d29cf3fb
VineelaChallagulla <vineelachallagulla@gmail.com>
2017-04-25 17:36:46 -0700

    Create Variable.java

---
+++ src/main/java/umlparser/Variable.java
@@ -0,0 +1,33 @@
+package umlparser;
+
+public class Variable {
+
+	String name;
+	String type;
+	String modifier;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getModifier() {
+		return modifier;
+	}
+
+	public void setModifier(String modifier) {
+		this.modifier = modifier;
+	}
+
+}
