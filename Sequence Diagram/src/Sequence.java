ba7174f5aaa4d2e5f2c69e7454ef67a9d774bedc
VineelaChallagulla <vineelachallagulla@gmail.com>
2017-05-06 23:44:26 -0700

    Create Sequence.java

---
+++ Sequence Diagram/src/Sequence.java
@@ -0,0 +1,49 @@
+
+public class Sequence {
+	
+	String sorceClassName;
+	String targetClassName;
+	String methodSignature;
+	String returnObject;
+	String exceptionType;
+	int Type;
+	
+	public String getSorceClassName() {
+		return sorceClassName;
+	}
+	public void setSorceClassName(String sorceClassName) {
+		this.sorceClassName = sorceClassName;
+	}
+	public String getTargetClassName() {
+		return targetClassName;
+	}
+	public void setTargetClassName(String targetClassName) {
+		this.targetClassName = targetClassName;
+	}
+	public String getMethodSignature() {
+		return methodSignature;
+	}
+	public void setMethodSignature(String methodSignature) {
+		this.methodSignature = methodSignature;
+	}
+	public String getReturnObject() {
+		return returnObject;
+	}
+	public void setReturnObject(String returnObject) {
+		this.returnObject = returnObject;
+	}
+	public int getType() {
+		return Type;
+	}
+	public void setType(int type) {
+		Type = type;
+	}
+	public String getExceptionType() {
+		return exceptionType;
+	}
+	public void setExceptionType(String exceptionType) {
+		this.exceptionType = exceptionType;
+	}
+	
+		
+}
